6c2fcd97ceac7d9b36dbeb502ef3ffbed5fc6251
LindaKadz <achielinda44@gmail.com>
2018-08-11 23:28:48 +0300

    create contact page from test and initialize guard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "Hey there! An awesome app is coming soon. Stay tuned!"
+    #render html: "Hey there! An awesome app is coming soon. Stay tuned!"
   end
 end
