537e19266eb2186449504b01d43abf2665400a4c
LindaKadz <achielinda44@gmail.com>
2018-09-05 12:29:16 +0300

    Successful signup and tests

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,4 +10,15 @@ test "invalid sign up" do
   assert_select 'div#<CSS id for error explanation>'
   assert_select 'div.<CSS class for field with error>'
 end
+
+test "valid sign up" do
+  get signup_path
+  assert_difference "User.count", 1 do
+    post users_path, params: { user: { name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password" } }
+  end
+  follow_redirect!
+  assert_template "users/show"
+  assert_not flash.empty?
+end
+
 end
